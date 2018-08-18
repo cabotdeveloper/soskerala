82a4efa290a38e79b927f6efb1bb253115c88632
Vimal Maheedharan <vimal.maheedharan@cabotsolutions.com>
2018-08-18 17:08:38 +0530

    Changed logic to show correct address

--- ajax/add_issue.php
+++ ajax/add_issue.php
@@ -10,7 +10,8 @@ if($_POST ){
     $data = $_POST;
     $db = new SQLite3('../db/rescueDb.db');
     $cordinates = get_geocode($data['location']);
-    $location = ($cordinates['formatted_address'])?$cordinates['formatted_address']:$data['location'];
+    // $location = ($cordinates['formatted_address'])?$cordinates['formatted_address']:$data['location'];
+    $location = $data['location'];
     $rptTime = date('d-M-Y H:i:s', time());
     $db->query("BEGIN TRANSACTION");
     $sql = "insert into 'issues' ('latitude','longitude','location_address','no_of_persons','contact_person_name','contact_person_mobile','additional_notes','issue_status','reported_date','updated_date') VALUES('".$cordinates['latitude']."','".$cordinates['longitude']."','".$location."','".$data['noPersons']."','".$data['contactName']."','".$data['contactMobile']."','".$data['notes']."',0,'".$rptTime."','".$rptTime."')";    
