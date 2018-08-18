a88b1a37e87a9ee75a45965c8e0af03f3d1628e5
vineeth <vineeth.kumar@cabotsolutions.com>
2018-08-19 00:02:46 +0530

    minor chnages

--- js/main.js
+++ js/main.js
@@ -194,7 +194,7 @@ $(document).ready(function() {
                 } else if (row.issue_status == 1) {
                     return "Rescue In Progress";
                 } else {
-                    return "Yet to rescue";
+                    return "To be rescued";
                 }
 
             },
