8acb95c122e8fca5c9af0075fd711ddbe432448a
Vimal Maheedharan <vimal.maheedharan@cabotsolutions.com>
2018-08-18 21:10:12 +0530

    Bug fix

--- ajax/change_status.php
+++ ajax/change_status.php
@@ -1,19 +1,21 @@
 <?php
+session_start();
 if (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'localhost') {
     ini_set('display_errors', 'On');
     error_reporting(E_ALL);
 }
-session_start();
-if(isset($_SESSION) && $_SESSION["user_type"] == 1){
+
+if(isset($_SESSION["user_type"]) && ($_SESSION["user_type"] == 2 || $_SESSION["user_type"] == 3)) {
+    if (isset($_GET['issue_id']) && isset($_GET['newStatus'])) {
+        $db = new SQLite3('../db/rescueDb.db');
+        $db->query("BEGIN TRANSACTION");
+        $db->query("UPDATE  issues SET issue_status = '".$_GET['newStatus']."' WHERE issue_id='".$_GET['issue_id']."'");
+        $db->query("COMMIT");
+        echo 1;
+    } else {
+        echo 0;
+    }
+} else {    
     echo 2;
     exit;
-}
-else if (isset($_GET['issue_id']) && isset($_GET['newStatus'])){
-    $db = new SQLite3('../db/rescueDb.db');
-    $db->query("BEGIN TRANSACTION");
-    $db->query("UPDATE  issues SET issue_status = '".$_GET['newStatus']."' WHERE issue_id='".$_GET['issue_id']."'");
-    $db->query("COMMIT");
-    echo 1;
-} else {
-    echo 0;
-}
+} 
